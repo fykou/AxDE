4bab4743dd064a8153ef5759be655c6a2c701d08
fykou <54535198+fykou@users.noreply.github.com>
2025-05-21 19:48:52 +0200

    flat

--- Scripts/install_pst.sh
+++ Scripts/install_pst.sh
@@ -1,38 +1,34 @@
 #!/usr/bin/env bash
 
-scrDir=$(dirname "$(realpath "$0")")
-
-if ! source "${scrDir}/global_fn.sh"; then
+if ! source "${SCR_DIR}/global_fn.sh"; then
     echo "Error: unable to source global_fn.sh..."
     exit 1
 fi
 
-cloneDir="${cloneDir:-$CLONE_DIR}"
+# # dolphin
+# if pkg_installed dolphin && pkg_installed xdg-utils; then
+#     print_log -c "[FILEMANAGER] " -b "detected :: " "dolphin"
+#     xdg-mime default org.kde.dolphin.desktop inode/directory
+#     print_log -g "[FILEMANAGER] " -b " :: " "setting $(xdg-mime query default "inode/directory") as default file explorer..."
 
-# dolphin
-if pkg_installed dolphin && pkg_installed xdg-utils; then
-    print_log -c "[FILEMANAGER] " -b "detected :: " "dolphin"
-    xdg-mime default org.kde.dolphin.desktop inode/directory
-    print_log -g "[FILEMANAGER] " -b " :: " "setting $(xdg-mime query default "inode/directory") as default file explorer..."
+# else
+#     print_log -y "[FILEMANAGER] " -b " :: " "dolphin is not installed..."
+#     print_log -y "[FILEMANAGER] " -b " :: " "Setting $(xdg-mime query default "inode/directory") as default file explorer..."
+# fi
 
-else
-    print_log -y "[FILEMANAGER] " -b " :: " "dolphin is not installed..."
-    print_log -y "[FILEMANAGER] " -b " :: " "Setting $(xdg-mime query default "inode/directory") as default file explorer..."
-fi
-
-# shell
-"${scrDir}/restore_shl.sh"
+# # shell
+# "${SCR_DIR}/restore_shl.sh"
 
 # flatpak
 if ! pkg_installed flatpak; then
     print_log -r "[FLATPAK]" -b "list :: " "flatpak application"
-    awk -F '#' '$1 != "" {print "["++count"]", $1}' "${scrDir}/extra/custom_flat.lst"
+    awk -F '#' '$1 != "" {print "["++count"]", $1}' "${SCR_DIR}/extra/custom_flat.lst"
     prompt_timer 60 "Install these flatpaks? [Y/n]"
     fpkopt=${PROMPT_INPUT,,}
 
     if [ "${fpkopt}" = "y" ]; then
         print_log -g "[FLATPAK]" -b "install :: " "flatpaks"
-        "${scrDir}/extra/install_fpk.sh"
+        "${SCR_DIR}/extra/install_fpk.sh"
     else
         print_log -y "[FLATPAK]" -b "skip :: " "flatpak installation"
     fi
